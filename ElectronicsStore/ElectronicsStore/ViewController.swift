feacf327c1c2ba27795313682a409dae1000548d
Алена Карпова <alenapancenko00@gmail.com>
2022-10-06 12:08:46 +0300

    init()

--- ElectronicsStore/ElectronicsStore/ViewController.swift
+++ ElectronicsStore/ElectronicsStore/ViewController.swift
@@ -8,17 +8,26 @@
 import UIKit
 ///
 class ViewController: UIViewController {
+  
     // MARK: - Constants
+    
     // MARK: - Private IBoutlet
+    
     // MARK: - Private Visual Components
+    
     // MARK: - Public Properties
+    
     // MARK: - Private Properties
+    
     // MARK: - Initializers
+    
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     // MARK: - Public Methods
+    
     // MARK: - Private Action
+    
     // MARK: - Private Methods
 }
