4572e06b0596410539fdfccf71fd8374a9c5e913
Алена Карпова <alenapancenko00@gmail.com>
2022-10-06 12:06:32 +0300

    init()

---
+++ ElectronicsStore/ElectronicsStore/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ElectronicsStore
+//
+//  Created by Алена Панченко on 06.10.2022.
+//
+
+import UIKit
+///
+class ViewController: UIViewController {
+    // MARK: - Constants
+    // MARK: - Private IBoutlet
+    // MARK: - Private Visual Components
+    // MARK: - Public Properties
+    // MARK: - Private Properties
+    // MARK: - Initializers
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    // MARK: - Public Methods
+    // MARK: - Private Action
+    // MARK: - Private Methods
+}
