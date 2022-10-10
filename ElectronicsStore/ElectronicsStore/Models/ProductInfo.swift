b41c8ce29d45aca875e9e541bd317b546c2d306b
Алена Карпова <alenapancenko00@gmail.com>
2022-10-10 16:38:45 +0300

    Сделала 17 урок

---
+++ ElectronicsStore/ElectronicsStore/Models/ProductInfo.swift
@@ -0,0 +1,14 @@
+//
+//  ProductInfo.swift
+//  ElectronicsStore
+//
+//  Created by Алена Панченко on 10.10.2022.
+//
+
+import Foundation
+/// Модель продукта
+struct ProductInfo {
+    var productName: String = ""
+    var productImageNames: [String] = []
+    var productPrice: String = ""
+}
