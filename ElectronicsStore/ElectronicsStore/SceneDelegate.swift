f67ae86ae1b353a7d740bf62a27c9acdac1c795d
Алена Карпова <alenapancenko00@gmail.com>
2022-10-14 12:44:41 +0300

    no message

--- ElectronicsStore/ElectronicsStore/SceneDelegate.swift
+++ ElectronicsStore/ElectronicsStore/SceneDelegate.swift
@@ -15,12 +15,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = scene as? UIWindowScene else { return }
                 window?.windowScene = scene
-                let userDefaults = UserDefaults.standard
-                guard userDefaults.object(forKey: "didShow") != nil else {
-                    window?.rootViewController = StartPageViewController()
-                    window?.makeKeyAndVisible()
-                    return
-                }
                 window?.rootViewController = MainTabBarController()
                 window?.makeKeyAndVisible()
     }
